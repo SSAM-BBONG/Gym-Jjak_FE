59e3d4d7365aced6da40a806080ca110456f63ae
sieun2im <kse040531@naver.com>
2026-05-27 13:06:54 +0900

    [FIX] 관리자 페이지 신고 유형을 사유 확인 버튼으로 수정

--- src/feature/admin/components/ReportReason.tsx
+++ src/feature/admin/components/ReportReason.tsx
@@ -1,9 +1,5 @@
 'use client'
 
-import useModal from "@/components/hooks/useModal";
-import CheckReportModal from "./modals/CheckReportModal";
-import TwoButtonModal from "@/components/ui/TwoButtonModal";
-
 interface ActiveStatusProps {
     text: '욕설' | '광고' | '음란물' | '사기' | '허위 정보'
 }
@@ -17,47 +13,14 @@ const colorVariants = {
 };
 
 export default function ReportReason({ text }: ActiveStatusProps) {
-    const handleConfirm1 = () => {
-        ApprovalModal.openModal();
-    };
-
-    const handleReject = () => {
-        RejectModal.openModal();
-    }
-
-    const modal = useModal(handleConfirm1, handleReject);
-    const ApprovalModal = useModal(handleConfirm1);
-    const RejectModal = useModal(handleConfirm1);
-
     return (
         <>
             <div
-                onClick={modal.openModal}
                 className={`py-1 px-3 font-medium text-xs rounded-lg border inline ${colorVariants[text]}`}>
                 {text}
 
             </div>
-            <CheckReportModal
-                isModal={modal.isModal}
-                closeModal={modal.closeModal}
-                activeModal={modal.activeModal}
-                noneActiveModal={modal.noneActiveModal}
-                title='모달입니다'
-            />
-            <TwoButtonModal
-                isModal={ApprovalModal.isModal}
-                closeModal={ApprovalModal.closeModal}
-                activeModal={ApprovalModal.activeModal}
-                title='신고 승인'
-                content={`신고를 승인하시겠습니까?`}
-            />
-            <TwoButtonModal
-                isModal={RejectModal.isModal}
-                closeModal={RejectModal.closeModal}
-                activeModal={RejectModal.activeModal}
-                title='신고 승인'
-                content={`신고를 반려하시겠습니까?`}
-            />
+
         </>
 
     );
