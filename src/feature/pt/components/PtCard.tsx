289ee04014b6689b97dc1280cdec3c1e35e507dd
biyuns <cart0089@naver.com>
2026-05-28 21:34:38 +0900

    [FEATURE] PT ZONE 메인페이지 구현#43

--- src/feature/pt/components/PtCard.tsx
+++ src/feature/pt/components/PtCard.tsx
@@ -1,5 +1,28 @@
-export default function Page() {
+import Link from "next/link";
+
+interface PtCardProps {
+    imgsrc : string,
+    title : string,
+    content : string,
+    movecoment : string,
+    move : string
+}
+
+export default function PtCard({imgsrc, title, content, movecoment, move}: PtCardProps) {
     return (
-        <div></div>
+        <Link href={move}>
+        <div className="
+        flex flex-col gap-3 
+        items-start
+        border border-[#36415380] rounded-[16px]
+        bg-[linear-gradient(135deg,rgba(16,24,40,0.90)0%,rgba(30,41,57,0.90)100%)]
+        hover:border-[#BFFF0B66] hover:bg-[linear-gradient(135deg,rgba(191,255,11,0.20)0%,rgba(168,230,0,0.10)100%)]
+        p-6 ">
+            <img src={imgsrc} alt="PT ZONE 카드 이미지"/>
+            <p className="text-[24px] font-black text-white"> {title} </p>
+            <p className="text-[14px] font-normal text-[#D1D5DC]"> {content} </p>
+            <p className="text-[14px] font-extrabold text-[#99A1AF] hover:text-[#BFFF0B]"> {movecoment} → </p>
+        </div>
+        </Link>
     );
 }
--- src/feature/pt/components/PtPopularCard.tsx
+++ src/feature/pt/components/PtPopularCard.tsx
@@ -1,5 +1,27 @@
-export default function Page() {
+import { PtfindTestImg } from "@/components/ui/image";
+
+export default function PtPopularCard() {
     return (
-        <div></div>
+        <div className="
+        relative
+        flex flex-col
+        border border-[#1E2939] rounded-[14px]
+        bg-[#101828]
+        overflow-hidden
+        ">
+            <p className="absolute rounded-[4px] bg-[#BFFF0B] px-2 py-1 text-[12px] font-extrabold text-black top-2 left-2"> 태그 </p>
+            <img src={PtfindTestImg} width={320} height={160} alt="PT ZONE 인기강습 이미지"/>
+            <div className="flex flex-col p-4">
+                <div className="flex justify-between">
+                    <p className="text-[18px] font-extrabold text-white"> 가슴 집중 PT</p>
+                    <p className="text-[12px] font-extrabold text-black p-1 rounded-[4px] bg-[#BFFF0B]"> 카테고리 </p>
+                </div> 
+                <p className="text-[12px] font-normal text-[#99A1AF]"> 김철수 트레이너 </p>
+                <div className="flex justify-between">
+                    <p className="text-[12px] font-normal text-[#6A7282]"> 헬스장 주소 </p>
+                    <p className="text-[14px] font-extrabold text-[#BFFF0B]"> 50,000원 </p>
+                </div>
+            </div>
+        </div>
     );
 }
