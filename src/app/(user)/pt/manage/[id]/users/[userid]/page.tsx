91c0c762e59561d26f0f889657e57bd83eb35552
biyuns <cart0089@naver.com>
2026-05-29 12:13:43 +0900

    [FEATURE] PT ZONE - PT관리 - 회원조회 - 피드 관리 페이지 구현#60

--- src/app/(user)/pt/manage/[id]/users/[userid]/page.tsx
+++ src/app/(user)/pt/manage/[id]/users/[userid]/page.tsx
@@ -1,5 +1,53 @@
-export default function Page() {
+import PtManageFeedBackCard from "@/feature/pt/components/PtManageFeedBackCard";
+
+export default function PtManageUserFeedBackPage() {
     return (
-        <div></div>
+        <div className="flex flex-col gap-5 px-70 py-10">
+            <div className="
+            flex flex-col gap-4
+            bg-[linear-gradient(135deg,rgba(16,24,40,0.90)0%,rgba(30,41,57,0.90)100%)]
+            border border-[#36415380] rounded-[16px]
+            p-6
+            ">
+                <div className="flex gap-6 items-start">
+                    <div className="size-20 flex-1 rounded-full border-[2px] border-[#BFFF0B]"></div>
+                    <div className="flex flex-9 flex-col gap-2 items-start">
+                        <p className="text-[24px] font-black text-white"> 운동초보</p>
+                        <p className="text-[14px] font-normal text-[#99A1AF]"> student@test.com </p>
+                        <p className="px-3 py-1 border border-[#BFFF0B4D] rounded-full bg-[#BFFF0B33] text-[12px] font-extrabold text-[#BFFF0B]"> 수강중</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-3">
+                    <div className="flex justify-between">
+                        <p className="text-[14px] font-normal text-[#99A1AF]"> 진행도 </p>
+                        <p className="text-[14px] font-extrabold text-[#BFFF0B]"> 3/12회 </p>
+                    </div>
+                    <div className="flex h-2 rounded-full bg-[#364153]">
+                        <p className="w-[30%] rounded-full bg-[#BFFF0B]"></p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col">
+                    <p className="text-[14px] font-normal text-[#6A7282]"> 강습 : <span className="text-[#99A1AF]"> 체계적인 강습 집중 PT </span></p>
+                    <p className="text-[14px] font-normal text-[#6A7282]"> 연락처 : <span className="text-[#99A1AF]"> 010-1234-5678 </span></p>
+                </div>
+            </div>
+
+            <div className="
+            grid grid-cols-3 gap-2
+            bg-[#101828]
+            border border-[#1E2939] rounded-[14px]
+            p-2
+            ">
+                <p className="py-3 text-[16px] font-extrabold text-black bg-[#BFFF0B] rounded-[10px] text-center"> 피드백 관리 </p>
+                <p className="py-3 text-[16px] font-extrabold text-[#99A1AF] rounded-[10px] text-center"> 운동일지 </p>
+                <p className="py-3 text-[16px] font-extrabold text-[#99A1AF] rounded-[10px] text-center"> 식단 관리 </p>
+            </div>
+
+            <PtManageFeedBackCard/>
+
+
+        </div>
     );
 }
