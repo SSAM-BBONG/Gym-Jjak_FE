fdd7b80d766f98a93b0ff58ab8f83fa0049fb61e
biyuns <cart0089@naver.com>
2026-05-29 10:56:53 +0900

    [FEATURE] PT ZONE - PT관리 - 회원 목록 조회#59

--- src/app/(user)/pt/manage/[id]/page.tsx
+++ src/app/(user)/pt/manage/[id]/page.tsx
@@ -1,5 +1,16 @@
-export default function Page() {
+import PtManageUserCard from "@/feature/pt/components/PtManageUserCard";
+
+export default function PtManageDetailPage() {
     return (
-        <div></div>
+        <div className="flex flex-col gap-1 px-60 py-10">
+            <p className="text-[36px] font-black text-white"> 체계적인 가슴 집중 PT </p>
+            <p className="text-[14px] font-normal text-[#99A1AF]"> 신청자 관리 및 피드백 제공 </p>
+
+            <div className="flex flex-col gap-4 mt-6">
+                <PtManageUserCard/>
+                <PtManageUserCard/>
+                <PtManageUserCard/>
+            </div>
+        </div>
     );
 }
