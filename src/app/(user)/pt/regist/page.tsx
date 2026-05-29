71e0bd3cfcd7a5fd63ef3d810eae951f44f5a8dc
biyuns <cart0089@naver.com>
2026-05-29 09:51:55 +0900

    [FEATURE] PT ZONE - PT등록 페이지 구현#49

--- src/app/(user)/pt/regist/page.tsx
+++ src/app/(user)/pt/regist/page.tsx
@@ -1,5 +1,12 @@
+import PtRegistForm from "@/feature/pt/components/PtRegistForm";
+
 export default function Page() {
     return (
-        <div></div>
+        <div className="flex flex-col gap-5 px-70 py-10">
+            <p> PT 등록 </p>
+            <p> 새로운 PT를 등록해주세요. </p>
+
+            <PtRegistForm/>
+        </div>
     );
 }
