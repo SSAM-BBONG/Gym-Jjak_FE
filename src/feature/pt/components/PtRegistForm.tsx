66385015387c908c6902ef60fc5b27c4fe2b6163
biyuns <cart0089@naver.com>
2026-05-29 10:01:07 +0900

    [FEATURE] PT ZONE - PT등록 페이지 구현#49

--- src/feature/pt/components/PtRegistForm.tsx
+++ src/feature/pt/components/PtRegistForm.tsx
@@ -2,7 +2,7 @@ import { OrganApplicationUpload} from "@/components/ui/image";
 
 export default function PtRegistForm() {
     return (
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 mt-6">
             <div className="
             flex flex-col gap-4
             bg-[#101828]
