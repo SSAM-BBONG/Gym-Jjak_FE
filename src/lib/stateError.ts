8354bd81b1b1777183c9382b72ccf060b978e4a8
sieun2im <kse040531@naver.com>
2026-05-30 15:04:41 +0900

    [HOTFIX] 로그인 에러, 리프레시토큰 해결#94#95

--- src/lib/stateError.ts
+++ src/lib/stateError.ts
@@ -6,18 +6,39 @@ import { cookies } from "next/headers";
 export const refreshGet = async () => {
     const base_url = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8081';
     try {
-        console.log('1');
-        const res = await axios.post(`${base_url}/api/token/reissue`,
-            {},
-            { withCredentials: true })
-        console.log('2');
-
         const cookieStore = await cookies();
+
+        const refreshToken = cookieStore.get('refreshToken')?.value
+        const res = await axios.post(
+            `${base_url}/api/token/reissue`,
+            {},
+            {
+                headers: {
+                    Cookie: `refreshToken=${refreshToken}`
+                }
+            }
+        );
+
         cookieStore.set('accessToken', res.data.data.accessToken, {
             httpOnly: true,   // 자바스크립트 접근 불가(xss 방지)
             maxAge: 60 * 15,   // 15분
             path: '/'
         })
+
+        const setCookieHeader = res.headers['set-cookie'];
+
+        if (setCookieHeader) {
+            const newRefreshToken = setCookieHeader.find((cookie) => cookie.startsWith('refreshToken='));
+            const newRefreshValue = newRefreshToken?.split(';')[0].replace('refreshToken=', '');
+
+            if (newRefreshValue) {
+                cookieStore.set('refreshToken', newRefreshValue, {
+                    httpOnly: true,
+                    maxAge: 60 * 60 * 3,
+                    path: '/'
+                });
+            }
+        }
 
         return res.data.data.accessToken;
     } catch (error) {
