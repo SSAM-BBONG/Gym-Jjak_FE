4a78a72eb48baa37687df8d2471408d042ba14eb
biyuns <cart0089@naver.com>
2026-06-24 15:03:44 +0900

    [FIX] 조직 신청 - 신청 폼 컴포넌트 분리#218

---
+++ src/feature/mypage/components/OrganizationRegistBusinessVerification.tsx
@@ -0,0 +1,63 @@
+import { OrganizationApplicationDetail } from "../type";
+
+interface OrganizationRegistFormProps {
+    // 신청 & 조회 모드
+    isReadOnly: boolean;
+    application?: OrganizationApplicationDetail;
+}
+
+export default function OrganizationRegistBusinessVerification({ application, isReadOnly }: OrganizationRegistFormProps) {
+    return (
+    <div className="
+        flex flex-col gap-6
+        bg-[linear-gradient(135deg,rgba(16,24,40,0.90)0%,rgba(30,41,57,0.90)100%)]
+        p-8
+        rounded-[16px]
+        ">
+            <p className="text-[20px] text-white font-extrabold"> 사업자 정보 검증 </p>
+            <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                    <label className="text-[14px] font-medium text-white"> 사업자등록번호 </label>
+                    <input 
+                        className="px-4 py-3 bg-[#1E2939] border border-[#364153] rounded-[10px] text-[16px] font-normal text-[#FFFFFF80] outline-none" 
+                        type="text"
+                        name="businessRegistrationNumber"
+                        defaultValue={application?.businessRegistrationNumber}
+                        disabled={isReadOnly}
+                        placeholder="0000000000 (10자리)"/>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label className="text-[14px] font-medium text-white"> 상호(사업자 이름) </label>
+                    <input 
+                        className="px-4 py-3 bg-[#1E2939] border border-[#364153] rounded-[10px] text-[16px] font-normal text-[#FFFFFF80] outline-none" 
+                        type="text"
+                        name="businessName"
+                        defaultValue={application?.businessName}
+                        disabled={isReadOnly}
+                        placeholder="ex) 엑티브펄스 PT센터"/>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label className="text-[14px] font-medium text-white"> 대표자 이름 </label>
+                    <input 
+                        className="px-4 py-3 bg-[#1E2939] border border-[#364153] rounded-[10px] text-[16px] font-normal text-[#FFFFFF80] outline-none" 
+                        type="text"
+                        name="representativeName" 
+                        defaultValue={application?.representativeName}
+                        disabled={isReadOnly}
+                        placeholder="ex) 홍길동"/>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label className="text-[14px] font-medium text-white"> 개업 일자 </label>
+                    <input 
+                        className="px-4 py-3 bg-[#1E2939] border border-[#364153] rounded-[10px] text-[16px] font-normal text-[#FFFFFF80] outline-none" 
+                        name="openingDate" type="date"
+                        defaultValue={application?.openingDate}
+                        disabled={isReadOnly}
+                        placeholder="ex) 20000000"/>
+                </div>
+            </div>
+            {/* 읽기모드일때는 검증 버튼 뜨지 않게 설정 */}
+            {!isReadOnly && <button className="bg-[#BFFF0B] opacity-50 rounded-[10px] py-3 text-[16px] font-extrabold text-black"> 사업자 정보 검증하기 </button>}
+        </div>
+    );
+}
