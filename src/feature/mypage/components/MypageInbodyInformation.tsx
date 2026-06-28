482ccfb95c42c46a476daccce26f229e454f3997
biyuns <cart0089@naver.com>
2026-06-28 19:23:37 +0900

    [FEATURE] 마이페이지 - 컴포넌트 분리#287

---
+++ src/feature/mypage/components/MypageInbodyInformation.tsx
@@ -0,0 +1,38 @@
+import { MypageInbody, MypageOnboarding } from "@/components/ui/image";
+
+export default function MypageInbodyInformation() {
+    return (
+         <div
+                className="
+          flex flex-col
+          bg-[linear-gradient(135deg,rgba(16,24,40,0.90)0%,rgba(30,41,57,0.90)100%)]
+          border border-[#36415380]
+          rounded-[16px]
+          p-6
+          gap-6
+          "
+              >
+                <div className="flex gap-2 items-center">
+                  <img src={MypageOnboarding} alt="마이페이지 인바디 정보" />
+                  <p className="text-[18px] font-extrabold text-white">
+                    인바디 정보
+                  </p>
+                </div>
+
+                <div className="flex flex-col items-center gap-3 bg-[#1E2939] p-6 rounded-[14px]">
+                  <div className="flex items-center justify-center rounded-full bg-[#BFFF0B1A] p-3">
+                    <img src={MypageInbody} alt="마이페이지 인바디 입력하기" />
+                  </div>
+                  <p className="text-[16px] font-extrabold text-white">
+                    인바디 입력하기
+                  </p>
+                  <p className="text-[12px] font-medium text-[#99A1AF]">
+                    키, 몸무게, 체지방률 등을 기록하세요
+                  </p>
+                  <button className="px-4 py-2 text-[14px] font-bold text-black bg-[#BFFF0B] rounded-[16px]">
+                    입력하기
+                  </button>
+                </div>
+              </div>
+    );
+}
---
+++ src/feature/mypage/components/MypageMyActivities.tsx
@@ -0,0 +1,34 @@
+import { MypageMyActivity } from "@/components/ui/image";
+import Link from "next/link";
+
+export default function MypageMyActivities() {
+    return (
+        <div
+                className="
+            flex flex-col
+            bg-[linear-gradient(135deg,rgba(16,24,40,0.90)0%,rgba(30,41,57,0.90)100%)]
+            border border-[#36415380]
+            rounded-[16px]
+            p-6
+            gap-6
+            "
+              >
+                <div className="flex gap-2 items-center">
+                  <img src={MypageMyActivity} alt="마이페이지 나의 활동" />
+                  <p className="text-[18px] font-extrabold text-white">나의 활동</p>
+                </div>
+                <Link href="/mypage/mypost">
+                  <div className="flex justify-between bg-[#1E2939] rounded-[10px] p-4">
+                    <p className="text-[14px] font-medium text-white">
+                      내가 작성한 게시글
+                    </p>
+                    <div className="flex gap-2">
+                      <p className="text-[12px] font-medium text-[#6A7282]"> 12개 </p>
+                      <p className="text-[12px] font-medium text-[#6A7282]"> 〉</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+
+    );
+}
