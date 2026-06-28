2a09d8630b64eac8cb7e33461287c081a0fdb7de
biyuns <cart0089@naver.com>
2026-06-28 19:45:21 +0900

    [FEATURE] 마이페이지 - 비밀번호 확인 모달 UI 구현#286

--- src/feature/mypage/components/PasswordCheckModal.tsx
+++ src/feature/mypage/components/PasswordCheckModal.tsx
@@ -1,5 +1,39 @@
-export default function Page() {
+interface PasswordCheckModalProps {
+    isModal: boolean;
+    closeModal: () => void;
+    activeModal?: () => void;
+}
+
+export default function PasswordCheckModal({ isModal, closeModal, activeModal}: PasswordCheckModalProps) {
+    if (!isModal) return null;
+
     return (
-        <div></div>
+        <section
+            className="z-999 bg-black/50 fixed top-0 left-0 w-screen h-screen"
+            onClick={closeModal} 
+        >
+            <div
+                className="bg-gradient-to-br from-[#101828] to-[#000] w-md h-52 rounded-2xl border border-[#1E2939] z-1000 fixed top-1/2 left-1/2 p-6 flex -translate-x-1/2 -translate-y-1/2 flex-col justify-between"
+                onClick={(e) => e.stopPropagation()}>
+                <article>
+                    <h3 className="font-bold text-xl mb-4 text-[#E8EAF0]">비밀번호 확인</h3>
+                  <input
+                        type="password"
+                        placeholder="현재 비밀번호를 입력하세요"
+                        className="border border-[#3364153] bg-[#1E2939] rounded-[10px]"
+                    />
+                </article>
+                <article className='c-modal-btn-ct'>
+                    <button
+                        type="button"
+                        onClick={() => { activeModal?.(); closeModal(); }}
+                        className='w-full flex pt-2 pb-3 justify-center items-center rounded-lg text-black text-center font-semibold text-base bg-[#BFFF0B]'
+                    >
+                        확인
+                    </button>
+                </article>
+            </div>
+        </section>
+
     );
 }
