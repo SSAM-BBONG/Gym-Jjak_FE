4e9167d02b8f41ccfda6cc452be0da28311501dc
biyuns <cart0089@naver.com>
2026-06-28 21:11:43 +0900

    [FEATURE] 마이페이지 - 회원탈퇴 기능 연결#294

--- src/feature/mypage/components/MypageAccountSettings.tsx
+++ src/feature/mypage/components/MypageAccountSettings.tsx
@@ -1,8 +1,20 @@
+'use client'
+
 import { MypageAccountSetting } from "@/components/ui/image";
 import Link from "next/link";
+import { deleteMyAccountAction } from "../action";
+import { useState } from "react";
+import PasswordCheckModal from "./PasswordCheckModal";
 
 export default function MypageAccountSettings() {
+
+    const [passwordCheckModal, setpasswordCheckModal] = useState(false);
+
+    const handleUserDelectionClick = async () => {
+      const result = await deleteMyAccountAction();
+    }
     return (
+      <>
          <div
                 className="
             flex flex-col
@@ -32,9 +44,18 @@ export default function MypageAccountSettings() {
                   <p className="text-[14px] font-medium text-[#99A1AF]">
                     회원 탈퇴
                   </p>
-                  <p className="text-[12px] font-medium text-[#6A7282]"> 〉 </p>
+                  <button 
+                    type="button"
+                    onClick={() => setpasswordCheckModal(true)}
+                    className="text-[12px] font-medium text-[#6A7282] hover:cursor-pointer"> 〉 </button>
                 </div>
               </div>
-        
+
+              <PasswordCheckModal
+                isModal={passwordCheckModal}
+                closeModal={() => setpasswordCheckModal(false)}
+                checkPassword={handleUserDelectionClick}
+              />
+      </>
     );
 }
