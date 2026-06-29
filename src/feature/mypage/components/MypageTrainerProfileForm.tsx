f9d4b1627d8558aa1afacdacad657edc409063fc
biyuns <cart0089@naver.com>
2026-06-29 11:47:54 +0900

    [FEATURE] 마이페이지 - 내 트레이너 프로필 수정 API 연결#317

--- src/feature/mypage/components/MypageTrainerProfileForm.tsx
+++ src/feature/mypage/components/MypageTrainerProfileForm.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { MypageInbody, TrainerEssentialQulificationIcon, TrainerProfileImgUpload } from "@/components/ui/image";
 import { MyTrainerProfileData } from "../type";
 import MypageTrainerProfile from "./MypageTrainerProfile";
@@ -5,47 +7,164 @@ import MypageTrainerEssentialQUlification from "./MypageTrainerEssendtialQulific
 import MypageTrainerProfileSelfIntroduction from "./MypageTrainerProfileSelfIntroduction";
 import MypageTrainerProfileAwardHistory from "./MypageTrainerProfileAwardHistory";
 import MypageTrainerQulification from "./MypageTrainerQulification";
+import { useState } from "react";
+import { editMyTrainerProfileInformationAction } from "../action";
+import { Resolver, SubmitHandler, useForm } from "react-hook-form";
+import { MypageTrainerProfileFormValue, mypageTrainerProfileSchema } from "@/lib/mypageTrainerProfileEditSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface MypageTrainerProfileFormProps {
     data : MyTrainerProfileData
 }
 
 export default function MypageTrainerProfileForm( {data}: MypageTrainerProfileFormProps) {
+    const [mode, setMode] = useState("read");
+    const isReadOnly = mode === "read";
+
+    console.log(isReadOnly)
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        reset,
+        formState: { errors, isSubmitting },
+    } = useForm<MypageTrainerProfileFormValue>({
+        resolver: zodResolver(
+        mypageTrainerProfileSchema
+        ) as Resolver<MypageTrainerProfileFormValue>,
+        defaultValues: {
+        profileImageFile: null,
+        profileImageAction: "KEEP",
+        additionalCertifications: data.certifications
+            .filter((item) => item.certificationType !== "ESSENTIAL")
+            .map((item) => item.name),
+        awardHistories: data.awards.map((item) => item.name),
+        introduction: data.introduction ?? "",
+        },
+        mode: "onSubmit",
+    });
+
+    const handleEditClick = () => {
+        setMode("edit");
+    };
+
+    const handleCancelClick = () => {
+        reset({
+        profileImageFile: null,
+        profileImageAction: "KEEP",
+        additionalCertifications: data.certifications
+            .filter((item) => item.certificationType !== "ESSENTIAL")
+            .map((item) => item.name),
+        awardHistories: data.awards.map((item) => item.name),
+        introduction: data.introduction ?? "",
+        });
+
+        setMode("read");
+    };
+
+    const onSubmit: SubmitHandler<MypageTrainerProfileFormValue> = async (
+        values
+    ) => {
+        const formData = new FormData();
+
+        if (values.profileImageFile) {
+        formData.append("profileImageFile", values.profileImageFile);
+        }
+
+        formData.append("profileImageAction", values.profileImageAction);
+        formData.append("additionalCertifications", JSON.stringify(values.additionalCertifications));
+        formData.append("awardHistories", JSON.stringify(values.awardHistories));
+        formData.append("introduction", values.introduction);
+
+        const result = await editMyTrainerProfileInformationAction(formData);
+
+        if (!result.success) {
+        return;
+        }
+
+        setMode("read");
+    };
+
+    
     return (
-        <div className="flex flex-col px-40 pt-10">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col px-40 pt-10">
             <div className="flex justify-between items-center">
                 <div className="flex flex-col gap-3">
                     <p className="text-[36px] font-black text-white"> 트레이너 프로필 수정</p>
                     <p className="text-[14px] font-normal text-[#99A1AF]"> 트레이너 정보를 수정하세요</p>
                 </div>
-                <button 
-                    type="button"
-                    className="bg-[#BFFF0B] text-black text-[16px] px-4 py-2 rounded-[10px] font-bold"> 수정하기 </button>
+
+                {isReadOnly
+                    ? 
+                    (
+                    <button 
+                        className="bg-[#BFFF0B] text-black text-[16px] px-4 py-2 rounded-[10px] font-bold"
+                        type="button" 
+                        onClick={handleEditClick}
+                    >
+                        수정하기
+                    </button>
+                    ) 
+                    : 
+                    (
+                    <div className="flex gap-3">
+                        <button 
+                            className="bg-[#1E2939] text-white text-[16px] px-4 py-2 rounded-[10px] font-bold"
+                            type="button" 
+                            onClick={handleCancelClick} 
+                            disabled={isSubmitting}>
+                            취소
+                        </button>
+                        <button 
+                            className="bg-[#BFFF0B] text-black text-[16px] px-4 py-2 rounded-[10px] font-bold"
+                            type="submit" 
+                            disabled={isSubmitting}>
+                            {isSubmitting ? "수정 중..." : "수정하기"}
+                        </button>
+                    </div>
+                    )}
             </div>
 
             <div className="flex flex-col gap-6 mt-6">
     
             <MypageTrainerProfile 
                 data={data}
+                mode={mode}
+                setValue={setValue}
+                error={errors.profileImageFile?.message}
             />
 
             <MypageTrainerEssentialQUlification
                 data={data}
+                isReadOnly={true}
             />
 
             <MypageTrainerQulification
                 data={data}
+                mode={mode}
+                setValue={setValue}
+                error={errors.additionalCertifications?.message}
             />
 
             <MypageTrainerProfileAwardHistory
                 data={data}
+                mode={mode}
+                setValue={setValue}
+                error={errors.awardHistories?.message}
             />
 
             <MypageTrainerProfileSelfIntroduction
                 data={data}
+                mode={mode}
+                register={register}
+                error={errors.introduction?.message}
             />
-            <button className="bg-[#BFFF0B] text-black text-[16px] font-extrabold py-3 mb-20 rounded-[10px]"> 수정하기 </button>
+            <button 
+                type="submit"
+                disabled={isSubmitting}
+                className="bg-[#BFFF0B] text-black text-[16px] font-extrabold py-3 mb-20 rounded-[10px]"> 수정하기 </button>
             </div>
-        </div>
+        </form>
     );
 }
--- src/feature/mypage/components/MypageTrainerProfileSelfIntroduction.tsx
+++ src/feature/mypage/components/MypageTrainerProfileSelfIntroduction.tsx
@@ -1,6 +1,20 @@
-export default function MypageTrainerProfileSelfIntroduction({ data}) {
+import { MypageTrainerProfileFormValue } from "@/lib/mypageTrainerProfileEditSchema";
+import { UseFormRegister } from "react-hook-form";
+import { MyTrainerProfileData } from "../type";
+
+interface MypageTrainerProfileSelfIntroductionProps {
+  data: MyTrainerProfileData;
+  mode: string;
+  register: UseFormRegister<MypageTrainerProfileFormValue>;
+  error?: string;
+}
+
+export default function MypageTrainerProfileSelfIntroduction({ data, mode, register, error}: MypageTrainerProfileSelfIntroductionProps) {
+    
+    const isReadOnly = mode === "read"
+
     return (
-                    <div className="
+        <div className="
             flex flex-col gap-4
             p-8
             bg-[linear-gradient(135deg,rgba(16,24,40,0.90)0%,rgba(30,41,57,0.90)100%)]
@@ -9,10 +23,15 @@ export default function MypageTrainerProfileSelfIntroduction({ data}) {
             rounded-[16px]">
                 <p className="text-[20px] font-extrabold text-white"> 자기소개 </p>
                 <textarea 
-                    rows={10} cols={10} 
+                    rows={10} 
+                    cols={10} 
+                    disabled={isReadOnly}
                     defaultValue={data.introduction}
                     placeholder="자신을 소개하고, 어떤 트레이닝을 제공할 수 있는지 설명해주세요."
-                    className="bg-[#1E2939] border border-[#364153] p-4 text-white outline-none rounded-[10px]"/>
+                    className="bg-[#1E2939] border border-[#364153] p-4 text-white outline-none rounded-[10px]"
+                    {...register("introduction")}    
+                />
+                {error && <p className="text-sm text-red-500">{error}</p>}
             </div>
 
     );
