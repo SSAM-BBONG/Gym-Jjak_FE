1947c57aa1cf49967ca284737ee8761f4bfced70
sieun2im <kse040531@naver.com>
2026-07-07 19:21:23 +0900

    [FEATURE] 커뮤니티 반응형#461

--- src/app/(user)/community/[id]/edit/page.tsx
+++ src/app/(user)/community/[id]/edit/page.tsx
@@ -1,5 +1,50 @@
-export default function Page() {
+import { CommuDetailDeclaration, CommuDetailEdit, CommuDetailLike, CommuDetailRemove } from "@/components/ui/image";
+import CommuCommentCard from "@/feature/community/components/CommuCommentCard";
+import Image from "next/image";
+
+export default function page() {
     return (
-        <div></div>
+        <div className="flex flex-col gap-3 
+        md:bg-[linear-gradient(135deg,_rgba(16,24,40,0.90)_0%,_rgba(30,41,57,0.90)_100%)]
+        md:border-[#36415380] md:border-[1px]
+        md:rounded-[16px]
+        sm:mx-10
+        md:mx-25
+        lg:mx-40
+        mt-5
+        md:mt-10
+        p-8
+        ">
+            <h2
+                className="
+                flex items-center justify-center
+                self-baseline
+                rounded-[4px] px-3 py-1 
+                text-[#D1D5DC] bg-[#364153]
+                text-[12px]
+                font-extrabold"
+            >
+                자유게시판
+            </h2>
+            <input
+                placeholder="제목을 입력해주세요"
+                className="text-[22px] md:text-[30px] font-black text-white focus:outline-none" />
+
+            <div className="flex justify-between mt-5 md:my-5 items-center">
+                <div className="flex flex-col md:flex-row gap-3">
+                    <p className="text-[10px] md:text-[12px] lg:text-[14px] text-white font-medium"> 사용자 아이디</p>
+                    <p className="text-[10px] md:text-[12px] lg:text-[14px] text-[#99A1AF] font-normal">
+                        2025-05-13 14:30
+                    </p>
+                </div>
+            </div>
+            <hr className="border-[#1E2939]" />
+
+            <textarea
+                placeholder="내용을 입력해주세요"
+                defaultValue={'게시글 내용'}
+                className="rounded-[10px] h-47 focus:outline-none resize-none text-[13px] md:text-[16px] font-normal text-[#D1D5DC] p-2" />
+
+        </div>
     );
 }
--- src/app/(user)/community/[id]/page.tsx
+++ src/app/(user)/community/[id]/page.tsx
@@ -5,13 +5,17 @@ import Image from "next/image";
 export default function CommuDetailPage() {
   return (
     <div className="flex flex-col gap-3 
-    bg-[linear-gradient(135deg,_rgba(16,24,40,0.90)_0%,_rgba(30,41,57,0.90)_100%)]
-    border-[#36415380] border-[1px]
-    rounded-[16px]
-    mx-40
+    md:bg-[linear-gradient(135deg,_rgba(16,24,40,0.90)_0%,_rgba(30,41,57,0.90)_100%)]
+    md:border-[#36415380] md:border-[1px]
+    md:rounded-[16px]
+    sm:mx-10
+    md:mx-25
+    lg:mx-40
+    mt-5
+    md:mt-10
     p-8
     ">
-      <p
+      <h2
         className="
         flex items-center justify-center
         self-baseline
@@ -21,20 +25,20 @@ export default function CommuDetailPage() {
         font-extrabold"
       >
         자유게시판
-      </p>
-      <p className="text-[30px] font-black text-white"> 게시글 제목 </p>
+      </h2>
+      <h1 className="text-[22px] md:text-[30px] font-black text-white"> 게시글 제목 </h1>
 
-      <div className="flex justify-between my-5 items-center">
-        <div className="flex gap-3">
-          <p className="text-[14px] text-white font-medium"> 사용자 아이디</p>
-          <p className="text-[14px] text-[#99A1AF] font-normal">
+      <div className="flex justify-between mt-5 md:my-5 items-center">
+        <div className="flex flex-col md:flex-row gap-3">
+          <p className="text-[10px] md:text-[12px] lg:text-[14px] text-white font-medium"> 사용자 아이디</p>
+          <p className="text-[10px] md:text-[12px] lg:text-[14px] text-[#99A1AF] font-normal">
             2025-05-13 14:30
           </p>
-          <p className="text-[14px] text-[#99A1AF] font-normal"> 조회 160</p>
+          <p className="text-[10px] md:text-[12px] lg:text-[14px] text-[#99A1AF] font-normal"> 조회 160</p>
         </div>
         <div className="flex gap-3 items-center">
-          <div className=" flex gap-2 items-center self-center px-4 py-2 bg-[#1E2939] rounded-[10px] font-extrabold text-[16px] text-white">
-            <div className="relative w-4 h-4">
+          <div className=" flex gap-2 items-center self-center px-3 md:px-4 py-2 bg-[#1E2939] rounded-[10px] font-extrabold text-[10px] md:text-[14px] text-white">
+            <div className="relative w-3 h-3 md:w-4 md:h-4">
               <Image
                 src={CommuDetailLike}
                 alt="커뮤니티 상세조회 좋아요"
@@ -47,7 +51,7 @@ export default function CommuDetailPage() {
           </div>
 
           <div className="px-2 py-2 bg-[#1E2939] rounded-[10px]">
-            <div className="relative w-4 h-4">
+            <div className="relative w-3 h-3 md:w-4 md:h-4">
               <Image
                 src={CommuDetailEdit}
                 alt="게시글 수정"
@@ -59,7 +63,7 @@ export default function CommuDetailPage() {
           </div>
 
           <div className="px-2 py-2 bg-[#1E2939] rounded-[10px]">
-            <div className="relative w-4 h-4">
+            <div className="relative w-3 h-3 md:w-4 md:h-4">
               <Image
                 src={CommuDetailRemove}
                 alt="게시글 삭제"
@@ -71,7 +75,7 @@ export default function CommuDetailPage() {
           </div>
 
           <div className="px-2 py-2 bg-[#1E2939] rounded-[10px]">
-            <div className="relative w-4 h-4">
+            <div className="relative w-3 h-3 md:w-4 md:h-4">
               <Image
                 src={CommuDetailDeclaration}
                 alt="게시글 신고"
@@ -83,16 +87,17 @@ export default function CommuDetailPage() {
           </div>
         </div>
       </div>
-      <hr className="border-[#1E2939]" />
+      <hr className="border-[#1E2939] hidden md:block" />
 
-      <p className="text-[16px] font-normal text-[#D1D5DC] py-8"> 게시글 내용 </p>
+      <p className="text-[13px] md:text-[16px] font-normal text-[#D1D5DC] py-8"> 게시글 내용 게시글 내용 게시글 내용 게시글 내용 게시글 내용 게시글 내용 게시글 내용 게시글 내용게시글 내용 게시글 내용 게시글 내용게시글 내용게시글 내용게시글 내용게시글 내용게시글 내용 </p>
 
       <hr className="border-[#1E2939]" />
 
       <div className="flex gap-3 items-center">
-        <p className="text-[18px] text-white font-extrabold"> 댓글</p>
-        <p className="text-[18px] text-[#BFFF0B] font-extrabold"> 3</p>
+        <p className="text-[14px] md:text-[18px] text-white font-extrabold"> 댓글</p>
+        <p className="text-[14px] md:text-[18px] text-[#BFFF0B] font-extrabold"> 3</p>
       </div>
+      <CommuCommentCard />
       <CommuCommentCard />
     </div>
   );
