1b7f5c114f41292dabb72ee6045f942692f1f0fe
sieun2im <kse040531@naver.com>
2026-07-23 00:52:28 +0900

    [FEATURE] ai 결제 service, action 작성#578

--- src/feature/payment/type.ts
+++ src/feature/payment/type.ts
@@ -20,3 +20,27 @@ export interface PtPurchaseStatusResponse {
   message: string;
   data: PtPurchaseStatusData;
 }
+
+
+export type AiPlanType = 'MONTHLY' | 'YEARLY';
+
+export interface AiPlan {
+  planType: AiPlanType;
+  price: number;
+}
+
+export interface AiPlans {
+  plans: AiPlan[];
+}
+
+export interface MyPlan {
+  planType: AiPlanType;
+  status: "ACTIVE" | "EXPIRED";
+  startedAt: string;
+  expiredAt: string;
+}
+
+export interface AiPlanRequest {
+  planType: AiPlanType;
+}
+
--- src/service/payment.service.ts
+++ src/service/payment.service.ts
@@ -1,8 +1,9 @@
 import {
+  AiPlanRequest,
   PtPaymentCreateResponse,
   PtPurchaseStatusResponse,
 } from "@/feature/payment/type";
-import { fetchWithAuth } from "@/lib/feth";
+import { fetchWithAuth, fetchWithoutAuth } from "@/lib/feth";
 import { getErrorMessage } from "@/lib/stateError";
 
 export const createPtPayment = async (
@@ -44,3 +45,55 @@ export const getPtPurchaseStatus = async (
 
   return response.json();
 };
+
+
+export const createSubscriptionsPayment = async (planType: AiPlanRequest) => {
+  const response = await fetchWithAuth(" /api/payments/subscriptions", {
+    method: "POST",
+    body: JSON.stringify(planType),
+  });
+
+  if (!response.ok) {
+    const message = await getErrorMessage(
+      response,
+      "구독 결제 요청에 실패하였습니다."
+    );
+
+    throw new Error(message);
+  }
+
+  return response.json();
+};
+
+export const getPlan = async () => {
+  const response = await fetchWithoutAuth('/api/subscriptions/plans');
+
+  if (!response.ok) {
+    const message = await getErrorMessage(
+      response,
+      "구독 플랜 조회에 실패하였습니다."
+    );
+
+    throw new Error(message);
+  }
+
+  return response.json();
+};
+
+
+export const getMyPlan = async () => {
+  const response = await fetchWithoutAuth('/api/subscriptions/me');
+
+  if (!response.ok) {
+    const message = await getErrorMessage(
+      response,
+      "내 구독 플랜 조회에 실패하였습니다."
+    );
+
+    throw new Error(message);
+  }
+
+  return response.json();
+};
+
+
