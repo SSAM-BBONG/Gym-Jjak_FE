a8e107d4a17d7f2fdcfa3e8c2a208a107406a741
sieun2im <kse040531@naver.com>
2026-07-26 00:33:43 +0900

    [FEATURE] 관리자 헤더 수정 #673

--- src/components/layout/AdminHeader.tsx
+++ src/components/layout/AdminHeader.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Logo } from "../ui/image";
 import AdminHeaderAuthArea from "./AdminHeaderAuthArea";
+import AdminMobileNavMenu from "./AdminMobileNavMenu";
 
 interface AdminHeaderProps {
     userInf?: MyTokenPayload;
@@ -11,23 +12,25 @@ interface AdminHeaderProps {
 export default function AdminHeader({ userInf }: AdminHeaderProps) {
     return (
         <header className="fixed top-0 left-0 z-9999 flex h-17.5 w-full items-center justify-between border-b border-b-[#1E2939] bg-black px-5 sm:px-10">
-            <Link href="/admin/dashboard/users" className="flex items-center gap-3">
-                <div className="flex size-10 items-center justify-center rounded-[10px]">
-                    <div className="relative h-10 w-10">
-                        <Image src={Logo} alt="GYMJJAK 로고" fill priority sizes="40px" className="object-cover" />
+            <div className="flex gap-2">
+                <AdminMobileNavMenu />
+                <Link href="/admin/dashboard/users" className="flex items-center gap-3">
+                    <div className="hidden sm:block flex size-10 items-center justify-center rounded-[10px]">
+                        <div className="relative h-10 w-10">
+                            <Image src={Logo} alt="GYMJJAK 로고" fill priority sizes="40px" className="object-cover" />
+                        </div>
                     </div>
-                </div>
-                <div className="flex items-center gap-2">
-                    <div>
-                        <p className="text-[12px] text-[#BFFF0B]">GYMJJAK</p>
-                        <p className="text-[10px] text-white">Fitness Platform</p>
+                    <div className="flex items-center gap-2">
+                        <div>
+                            <p className="text-[12px] text-[#BFFF0B]">GYMJJAK</p>
+                            <p className="text-[10px] text-white">Fitness Platform</p>
+                        </div>
+                        <span className="hidden rounded-full border border-[#BFFF0B]/30 bg-[#BFFF0B]/10 px-2.5 py-1 text-xs font-semibold text-[#BFFF0B] sm:inline">
+                            관리자 계정
+                        </span>
                     </div>
-                    <span className="hidden rounded-full border border-[#BFFF0B]/30 bg-[#BFFF0B]/10 px-2.5 py-1 text-xs font-semibold text-[#BFFF0B] sm:inline">
-                        관리자 계정
-                    </span>
-                </div>
-            </Link>
-
+                </Link>
+            </div>
             <div className="flex items-center gap-4 sm:gap-6">
                 <nav aria-label="관리자 계정 메뉴">
                     <Link
